393e7e0e2325cc6efdf3d2ad0094b1d53a18a1b2
Hoang-Minh <quataoxanh83@gmail.com>
2019-04-19 17:23:57 -0700

    Mvc setup, loading customer and movie from hard code values

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer{Id = 1, Name = "John Smith"},
+            new Customer{Id = 2, Name = "Mary William"}
+        };
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customerViewModel = new CustomerViewModel {Customers = customers};
+
+            return View(customerViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = customers.Find(x => x.Id == id);
+
+            if (customer == null) return HttpNotFound();
+            var customerViewModel = new CustomerViewModel{Customer = customer};
+
+            return View(customerViewModel);
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public List<Movie> movies = new List<Movie>
+        {
+            new Movie{Id = 1, Name = "Movie 1"},
+            new Movie{Id = 2, Name = "Movie 2"}
+        };
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movieViewController = new MovieViewModel {Movies = movies};
+
+            return View(movieViewController);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = movies.Find(x => x.Id == id);
+            if (movie == null) return HttpNotFound("No such movie");
+            var movieViewController = new MovieViewModel {Movie = movie};
+            return View(movieViewController);
+        }
+    }
+}
