5208a41e8f09455dc37e6e476f2e219fc2ab8556
Hoang-Minh <quataoxanh83@gmail.com>
2019-04-22 22:53:47 -0700

    Add Anti Forgery Token

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -30,6 +30,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
